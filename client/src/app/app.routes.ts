4305fc00c9110600dc6e936e46805c9809d65e3e
zd3n3 <zd3n3@VIVI>
2025-03-30 20:35:31 +0200

    add routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { NotesComponent } from './notes/notes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'todo', component: TodoComponent },
+  { path: 'schedule', component: ScheduleComponent },
+  { path: 'notifications', component: NotificationsComponent },
+  { path: 'notes', component: NotesComponent },
+  { path: '', redirectTo: '/todo', pathMatch: 'full' }
+];
