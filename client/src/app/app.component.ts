2fc4ced2056a59abe4945c2a4835029e1f2055f2
pribylzdenek <pribylzdenek@sssvt.cz>
2025-03-31 14:54:06 +0200

    asdf

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'client';
+  protected readonly RouterLinkActive = RouterLinkActive;
 }
