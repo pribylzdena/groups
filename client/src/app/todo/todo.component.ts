55bd554fc13124168039347bd9f2dd6e578186e4
pribylzdenek <pribylzdenek@sssvt.cz>
2025-03-31 16:12:49 +0200

    ngFor for tasks

--- client/src/app/todo/todo.component.ts
+++ client/src/app/todo/todo.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-todo',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss'
 })
 export class TodoComponent {
-
+  tasks = [
+    { color: '#ff7f7f' },
+    { color: '#ffaf87' },
+    { color: '#c4ff87' },
+    { color: '#87c4ff' },
+  ];
 }
