6d4e57cdc72f6be7d7170a65c2b1caf81018822e
honzicektomas <honzicketomas@sssvt.cz>
2025-04-02 13:27:38 +0200

    notification-detail1

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { TodoComponent } from './pages/todo/todo.component';
 import { ScheduleComponent } from './pages/schedule/schedule.component';
 import { NotificationsComponent } from './pages/notifications/notifications.component';
 import { NotesComponent } from './pages/notes/notes.component';
+import {NotificationDetailComponent} from './pages/notification-detail/notification-detail.component';
+
 
 export const routes: Routes = [
   { path: 'todo', component: TodoComponent },
   { path: 'schedule', component: ScheduleComponent },
   { path: 'notifications', component: NotificationsComponent },
   { path: 'notes', component: NotesComponent },
-  { path: '', redirectTo: '/todo', pathMatch: 'full' }
+  { path: '', redirectTo: '/todo', pathMatch: 'full' },
+  { path: 'notification-show', component: NotificationDetailComponent}
 ];
--- client/src/app/pages/notification-detail/notification-detail.component.ts
+++ client/src/app/pages/notification-detail/notification-detail.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification-detail',
-  imports: [],
+  standalone: true,
   templateUrl: './notification-detail.component.html',
-  styleUrl: './notification-detail.component.scss'
+  styleUrls: ['./notification-detail.component.scss']
 })
 export class NotificationDetailComponent {
+  showDetails = true;
 
+  hideDetailsPage() {
+    this.showDetails = false;
+  }
 }
