6d3323316e20ec746e45ef39d325b3761140a506
BuildTools <unconfigured@null.spigotmc.org>
2025-04-06 23:02:34 +0200

    nevim proste nervy jak dabel

--- client/src/app/pages/note-list/note-list.component.ts
+++ client/src/app/pages/note-list/note-list.component.ts
@@ -1,29 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-notes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './note-list.component.html',
   styleUrls: ['./note-list.component.scss']
 })
 export class NoteListComponent {
   notes = [
-    { color: '#ff7f7f' },
-    { color: '#ffaf87' },
-    { color: '#c4ff87' },
-    { color: '#87c4ff' },
-    { color: '#58d658' },
-    { color: '#87ffff' },
-    { color: '#b24d4d' },
-    { color: '#c4c458' },
-    { color: '#9087ff' },
-    { color: '#0047ab' },
-    { color: '#c487ff' },
-    { color: '#1f6030' },
-    { color: '#502450' },
-    { color: '#846a29' },
-    { color: '#ffffff' }
+    { id: 1, color: '#ff7f7f' },
+    { id: 2, color: '#ffaf87' },
+    { id: 3, color: '#c4ff87' },
+    { id: 4, color: '#87c4ff' },
+    { id: 5, color: '#58d658' },
+    { id: 6, color: '#87ffff' },
+    { id: 7, color: '#b24d4d' },
+    { id: 8, color: '#c4c458' },
+    { id: 9, color: '#9087ff' },
+    { id: 10, color: '#0047ab' },
+    { id: 11, color: '#c487ff' },
+    { id: 12, color: '#1f6030' },
+    { id: 13, color: '#502450' },
+    { id: 14, color: '#846a29' },
+    { id: 15, color: '#ffffff' }
   ];
+
 }
