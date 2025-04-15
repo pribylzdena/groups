e9ab9cad9da5a8b966b2530fb1d80ee96e0e0df5
honzicektomas <honzicektomas@sssvt.cz>
2025-04-15 23:07:01 +0200

    llll

---
+++ server/WebApplication1/MyContexct.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1
+{
+    public class MyContexct : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=mysqlstudenti.litv.sssvt.cz;database=3b1_honzicektomas_db2;user=honzicektomz;password=123456;SslMode=none");
+        }
+    }
+}
