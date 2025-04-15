3c4ee3aaa77e8b2275f2b946da36b1a09a8e7770
zd3n3 <zd3n3@VIVI>
2025-04-15 21:27:06 +0200

    asdf

--- server/WebApplication1/Controllers/WeatherForecastController.cs
+++ server/WebApplication1/Controllers/WeatherForecastController.cs
@@ -16,6 +16,7 @@ namespace WebApplication1.Controllers
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+            //asdfasdfasdf
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
