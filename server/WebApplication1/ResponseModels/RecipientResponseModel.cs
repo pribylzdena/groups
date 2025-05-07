fd8ab4e35172191891a73c9fb0f6ad4587f96ad0
pribylzdenek <pribylzdenek@L211PC27>
2025-05-07 09:32:33 +0200

    update findAll nofitications

---
+++ server/WebApplication1/ResponseModels/RecipientResponseModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+using WebApplication1.Models;
+
+namespace WebApplication1.ResponseModels
+{
+    public class RecipientResponseModel
+    {
+        public int id { get; set; }
+        public DateTime? readAt { get; set; }
+        public UserResponseModel user { get; set; }
+
+        public RecipientResponseModel(UsersNotification data, User user)
+        {
+            this.id = data.id;
+            this.readAt = data.read_at;
+            this.user = new UserResponseModel(user);
+        }
+    }
+}
